71ddab4d3a2d1adb2615e097280294201fac2ab3
joao vitor <joao3klb@hotmail.com>
2020-05-08 21:41:49 -0300

    Envio de email

---
+++ BackEnd/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  secure:false,
+  auth: {
+    user: "2322ddf87c6504",
+    pass: "265c6fd048d84d"
+  },
+  default:{
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  }
+};
