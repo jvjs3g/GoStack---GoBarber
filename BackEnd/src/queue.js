ba906671815742a29bce55c48c622e51703cae8b
joao vitor <joao3klb@hotmail.com>
2020-05-09 22:33:45 -0300

    Configurando fila com Redis 1.0

---
+++ BackEnd/src/queue.js
@@ -0,0 +1,6 @@
+import Queue from './lib/Queue';
+
+Queue.processQueue();
+
+// isso serve para que a fila nao interfira na performace da nossa aplicação.
+// ou seja podemos rodar em outro terminal as nossas filas e em outro o nosso servidor 
